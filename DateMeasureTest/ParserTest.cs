4d2ed3f6611021bf118625ca84afccbe46bb9420
Lukasz Bielenin <l.bielenin@gmail.com>
2017-11-05 17:31:50 +0100

    Implement test for converting DMY date with slash.

--- DateMeasureTest/ParserTest.cs
+++ DateMeasureTest/ParserTest.cs
@@ -21,5 +21,21 @@ namespace DateMeasureTest
             var parser = DateTime.TryParse(testString, out DateTime result);
             Assert.AreEqual(result.ToShortDateString(), "12.04.1994");
         }
+        
+        [Test]
+        public void TestIfParserConvertsDMYFormatWithSlashProperly()
+        {
+            var testString = "12/04/1994";
+            var parser = DateTime.TryParse(testString, out DateTime result);
+            Assert.AreEqual(result.ToShortDateString(), "12.04.1994");
+        }
+        
+        [Test]
+        public void TestIfParserConvertsYMDFormatWithDotProperly()
+        {
+            var testString = "1994.12.04";
+            var parser = DateTime.TryParse(testString, out DateTime result);
+            Assert.AreEqual(result.ToShortDateString(), "12.04.1994");
+        }
     }
 }
