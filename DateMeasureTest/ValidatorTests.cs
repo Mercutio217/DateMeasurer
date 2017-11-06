500f6e3a1028dcd985172fc58e4b0d74abb8030d
Lukasz Bielenin <l.bielenin@gmail.com>
2017-11-06 14:58:56 +0100

    Implement test for validator for invalid dates.

--- DateMeasureTest/ValidatorTests.cs
+++ DateMeasureTest/ValidatorTests.cs
@@ -1,17 +1,26 @@
-﻿using NUnit.Framework;
+﻿using DateMeasure;
+using NUnit.Framework;
 
 namespace DateMeasureTest
 {
     [TestFixture]
     public class ValidatorTests
     {
-        private Validator testValidator;
+        private Validator _testValidator;
         
         [Test]
         public void TestIfValidatorChecksValidDate()
         {
-            testValidator = new Validator();
-            Assert.True(testValidator.Validate("15.05.2001"));
+            _testValidator = new Validator("15.05.2001");
+            Assert.True(_testValidator.Validate());
+
+        }
+        
+        [Test]
+        public void TestIfValidatorChecksInValidDate()
+        {
+            _testValidator = new Validator("45.05.-1");
+            Assert.False(_testValidator.Validate());
 
         }
     }
