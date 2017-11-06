65ba475f017b6a6076760d6be554e9157abee6bd
Lukasz Bielenin <l.bielenin@gmail.com>
2017-11-06 14:53:41 +0100

    Create test for validate.

--- DateMeasureTest/ValidatorTests.cs
+++ DateMeasureTest/ValidatorTests.cs
@@ -5,6 +5,14 @@ namespace DateMeasureTest
     [TestFixture]
     public class ValidatorTests
     {
+        private Validator testValidator;
         
+        [Test]
+        public void TestIfValidatorChecksValidDate()
+        {
+            testValidator = new Validator();
+            Assert.True(testValidator.Validate("15.05.2001"));
+
+        }
     }
 }
