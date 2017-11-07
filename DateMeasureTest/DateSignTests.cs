f570b9b934f57b4a50fd6fd5cf075359eec7f9d2
Lukasz Bielenin <l.bielenin@gmail.com>
2017-11-07 19:51:01 +0100

    Implement tests for SetSeparator.

--- DateMeasureTest/DateSignTests.cs
+++ DateMeasureTest/DateSignTests.cs
@@ -1,10 +1,37 @@
 ﻿using NUnit.Framework;
+using DateMeasure;
 
 namespace DateMeasureTest
 {
     [TestFixture]
     public class DateSignTests
     {
+        private DateSign _testDateSign;
+
+        [Test]
+        public void TestIfDateSignConvertsDateProperly()
+        {
+            _testDateSign = new DateSign("12/04/1999");
+            Assert.True(_testDateSign.SetConvertedDate());
+        }
+
+        [Test]
+        public void TestIfDateSignGetsSignProperly1()
+        {
+            _testDateSign = new DateSign("14/11/1991");
+            _testDateSign.SetSeparator();
+            Assert.AreEqual("/", _testDateSign.Separator);
+        }
+        
+        [Test]
+        public void TestIfDateSignGetsSignProperly2()
+        {
+            _testDateSign = new DateSign("14-11-1991");
+            _testDateSign.SetSeparator();
+            Assert.AreEqual("-", _testDateSign.Separator);
+        }
+        
+        
         
     }
 }
