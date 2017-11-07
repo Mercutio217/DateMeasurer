210ddc00f12f6191fbdfceac59092afee95ba5d2
Lukasz Bielenin <l.bielenin@gmail.com>
2017-11-07 20:50:27 +0100

    Implement ControlFacade class.

---
+++ DateMeasureTest/DatePatternTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+namespace DateMeasureTest
+{
+    [TestFixture]
+    public class DatePatternTests
+    {
+        [Test]
+        public void TestIfComparatorWorksProperly()
+    }
+}
